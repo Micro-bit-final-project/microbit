e7b3af0d0c866cf8627f4b4c46a0914662af1c68
Giuseppe Barillari <joe2k01dev@gmail.com>
2021-04-05 09:39:43 +0100

    microbit: Add shift register related comments
    Signed-off-by: Giuseppe Barillari <joe2k01dev@gmail.com>

--- source/shift_register_helper.cpp
+++ source/shift_register_helper.cpp
@@ -10,7 +10,10 @@ MicroBitPin SHIFTREGISTER_DATA(MICROBIT_ID_IO_P15, MICROBIT_PIN_P15, PIN_CAPABIL
 int8_t lives = SHIFTREGISTER_BITS; // Number of lives / LEDs
 
 void shift_register_all_leds() {
-    // Light up all the LEDs
+    /*
+    This function lights up all the lives LEDs by interacting with
+    the shift register.
+    */
     SHIFTREGISTER_DATA.setDigitalValue(HIGH);
     SHIFREGISTER_CLOCK.setDigitalValue(LOW);
     for (int i = 0; i < SHIFTREGISTER_BITS; i++) {
@@ -22,7 +25,13 @@ void shift_register_all_leds() {
 }
 
 void decrease_lives(MicroBitEvent e) {
-
+    /*
+    This function is called whenever the decrease_lives_evt event
+    is fired. It interacts with the shift register to decrease the number
+    of turned on LEDs by one every time. The LED controlled by the
+    last register is the first to turn off.
+    - e: The MicroBitEvent passed to this function. Ignored.
+    */
     SHIFTREGISTER_DATA.setDigitalValue(LOW);
     SHIFREGISTER_CLOCK.setDigitalValue(LOW);
     for (int i = 0; i < SHIFTREGISTER_BITS; i++) {
